cc221fc212fcc9b964ec9feaeec9a97b9a00d82b
Dimaskes <d.imon-million@yandex.ru>
2020-11-29 14:11:25 +0400

    Компонента PizzaBlock

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import { Route } from 'react-router-dom';
 
 import { Header } from './components';
@@ -6,11 +8,26 @@ import { Home, Cart } from './pages';
 import './scss/app.scss';
 
 function App() {
+  const [pizzas, setPizzas] = useState([]);
+
+  useEffect(() => {
+    // <---Без использования библиотеки axios--->
+    // fetch('http://localhost:3000/db.json')
+    //   .then((resp) => resp.json())
+    //   .then((json) => {
+    //     setPizzas(json.pizzas);
+    //   });
+    // <---С использованием библиотеки axios--->
+    axios.get('http://localhost:3000/db.json').then(({ data }) => {
+      setPizzas(data.pizzas);
+    });
+  }, []);
+
   return (
     <div className='wrapper'>
       <Header />
       <div className='content'></div>
-      <Route exact path='/' component={Home} />
+      <Route exact path='/' render={() => <Home items={pizzas} />} />
       <Route exact path='/cart' component={Cart} />
     </div>
   );
