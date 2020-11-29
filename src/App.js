37ab0be77a406e2a268371c6ddf13e1eaffa0658
Dimaskes <d.imon-million@yandex.ru>
2020-11-29 18:20:22 +0400

    Вывод Pizza Block (классовая компонента) с использованием redux

--- src/App.js
+++ src/App.js
@@ -1,36 +1,69 @@
 import React, { useEffect, useState } from 'react';
+import { connect } from 'react-redux';
 import axios from 'axios';
-import { Route } from 'react-router-dom';
 
+import { Route } from 'react-router-dom';
 import { Header } from './components';
 import { Home, Cart } from './pages';
+import { setPizzas as setPizzasAction } from './redux/actions/pizzas';
 
 import './scss/app.scss';
 
-function App() {
-  const [pizzas, setPizzas] = useState([]);
+// function App() {
+//   useEffect(() => {
+//     // <---Без использования библиотеки axios--->
+//     // fetch('http://localhost:3000/db.json')
+//     //   .then((resp) => resp.json())
+//     //   .then((json) => {
+//     //     setPizzas(json.pizzas);
+//     //   });
+//     // <---С использованием библиотеки axios--->
+//     axios.get('http://localhost:3000/db.json').then(({ data }) => {
+//       setPizzas(data.pizzas);
+//     });
+//   }, []);
 
-  useEffect(() => {
-    // <---Без использования библиотеки axios--->
-    // fetch('http://localhost:3000/db.json')
-    //   .then((resp) => resp.json())
-    //   .then((json) => {
-    //     setPizzas(json.pizzas);
-    //   });
-    // <---С использованием библиотеки axios--->
+//   return (
+//     <div className='wrapper'>
+//       <Header />
+//       <div className='content'></div>
+//       <Route exact path='/' render={() => <Home items={pizzas} />} />
+//       <Route exact path='/cart' component={Cart} />
+//     </div>
+//   );
+// }
+
+class App extends React.Component {
+  componentDidMount() {
     axios.get('http://localhost:3000/db.json').then(({ data }) => {
-      setPizzas(data.pizzas);
+      this.props.setPizzas(data.pizzas);
     });
-  }, []);
+  }
 
-  return (
-    <div className='wrapper'>
-      <Header />
-      <div className='content'></div>
-      <Route exact path='/' render={() => <Home items={pizzas} />} />
-      <Route exact path='/cart' component={Cart} />
-    </div>
-  );
+  render() {
+    return (
+      <div className='wrapper'>
+        <Header />
+        <div className='content'></div>
+        <Route exact path='/' render={() => <Home items={this.props.items} />} />
+        <Route exact path='/cart' component={Cart} />
+      </div>
+    );
+  }
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return {
+    items: state.pizzas.items,
+    filters: state.filters,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    setPizzas: (items) => dispatch(setPizzasAction(items)),
+    dispatch,
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
